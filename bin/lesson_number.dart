c020c5e656015d2e817b4bfb91c5cf0e4361e13e
Amir Bahrawy <Amir@spark-sys.com>
2023-05-15 16:43:53 +0300

    Add hint

--- bin/lesson_number.dart
+++ bin/lesson_number.dart
@@ -1,5 +1,6 @@
 class LessonNumber {
   void call() {
     print('Welcome to implementation of functional programming in Dart');
+    print('Switch between branches to see the code of each lesson');
   }
 }
