1f4d86c95602f884d86260ea35b03f2c61acb2b5
Amir Bahrawy <Amir@spark-sys.com>
2023-05-16 14:56:19 +0300

    Finish lesson five

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,11 +1,19 @@
-int addOne(x) {
-  return x + 1;
+double addOne(double x) {
+  return x + 1.0;
 }
 
-int subtractTen(x) {
+double subtractTen(double x) {
   return x - 10;
 }
 
-int square(x) {
+double square(double x) {
   return x * x;
+}
+
+double Function(double) addOneSquareSubtractTen() {
+  return addOne.compose(square).compose(subtractTen);
+}
+
+extension Compose on Function {
+  T3 Function(T1) compose<T1, T2, T3>(T3 Function(T2) g) => (x) => g(this(x));
 }
--- bin/implementation.dart
+++ bin/implementation.dart
@@ -2,9 +2,17 @@ import 'functions.dart';
 
 // How to make a pipeline of functions in dart
 class Implementation {
-  List<int> data = [7, 4, 5, 6, 3, 8, 10];
+  List<double> data = [7, 4, 5, 6, 3, 8, 10];
 
   void call() {
+    //using declarative style
+    print('Declarative style');
     data.map(addOne).map(square).map(subtractTen).forEach(print);
+    //to avoid the creation of intermediate lists
+    print('Declarative style without intermediate lists');
+    data.map((x) => subtractTen(square(addOne(x)))).forEach(print);
+    //using composition
+    print('Using composition');
+    data.map(addOneSquareSubtractTen()).forEach(print);
   }
 }
