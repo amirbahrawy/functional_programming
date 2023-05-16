72c89cefccee4ea195a65bb723f5485c863cd85c
Amir Bahrawy <Amir@spark-sys.com>
2023-05-16 11:31:14 +0300

    Finish lesson four

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,11 +1,68 @@
-int addOne(x) {
-  return x + 1;
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+typedef Tuple<A, B> = MapEntry<A, B>;
+
+bool isAQualified(Order r) {
+  return true;
 }
 
-int subtractTen(x) {
-  return x - 10;
+double A(Order r) {
+  return 2.0;
 }
 
-int square(x) {
-  return x * x;
+bool isBQualified(Order r) {
+  return true;
+}
+
+double B(Order r) {
+  return 5.0;
+}
+
+bool isCQualified(Order r) {
+  return true;
+}
+
+double C(Order r) {
+  return 3.0;
+}
+
+double average(List<double> list) {
+  return list.reduce((a, b) => a + b) / list.length;
+}
+
+List<Tuple<bool Function(Order), double Function(Order)>> getDiscountRules() {
+  final discountRules = [
+    Tuple<bool Function(Order), double Function(Order)>(
+      isAQualified,
+      A,
+    ),
+    Tuple<bool Function(Order), double Function(Order)>(
+      isBQualified,
+      B,
+    ),
+    Tuple<bool Function(Order), double Function(Order)>(
+      isCQualified,
+      C,
+    ),
+  ];
+
+  return discountRules;
+}
+
+Order getOrderWithDiscount(
+  Order r,
+  List<Tuple<bool Function(Order), double Function(Order)>> rules,
+) {
+  //key: bool Function(Order) QualifyingCondition, value: double Function(Order) GetDiscount
+  final discount =
+      average(rules.where((rule) => rule.key(r)).map((a) => a.value(r)).toList()
+        ..sort()
+        ..take(3));
+  var newOrder = Order();
+  newOrder.discount = discount;
+  return newOrder;
+}
+
+class Order {
+  double discount;
+  Order({this.discount = 1.0});
 }
--- bin/implementation.dart
+++ bin/implementation.dart
@@ -1,6 +1,22 @@
+import 'functions.dart';
+
 class Implementation {
+  List<Order> ordersForProcessing = [
+    Order(discount: 5.0),
+    Order(discount: 10.0),
+    Order(discount: 15.0),
+    Order(discount: 20.0),
+    Order(discount: 25.0),
+    Order(discount: 30.0),
+    Order(discount: 35.0),
+    Order(discount: 40.0),
+    Order(discount: 45.0),
+    Order(discount: 50.0),
+    Order(discount: 55.0),
+  ];
   void call() {
-    print('Welcome to implementation of functional programming in Dart');
-    print('Switch between branches to see the code of each lesson');
+    ordersForProcessing
+        .map((order) => getOrderWithDiscount(order, getDiscountRules()))
+        .forEach((order) => print(order.discount));
   }
 }
