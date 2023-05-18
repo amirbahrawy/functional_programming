3e03bbf20fcee80b9b8a8a5e4b34df2c6feeff34
Amir Bahrawy <Amir@spark-sys.com>
2023-05-18 10:51:11 +0300

    Finish lesson nine

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,11 +1,10 @@
-int addOne(x) {
-  return x + 1;
-}
-
-int subtractTen(x) {
-  return x - 10;
-}
-
-int square(x) {
-  return x * x;
+// Closure as the inner function close over tax and basic salary
+// because there are many people have the same basic salary why calculate tax every time so we make closure
+//to prevent bugs (if we pass basic salary every time )
+//make function to save state (basic salary,tax) inside it
+double Function(double) grossSalaryCalculator(double basicSalary) {
+  final tax = 0.2 * basicSalary;
+  return (double bonus) {
+    return bonus + tax + basicSalary;
+  };
 }
--- bin/implementation.dart
+++ bin/implementation.dart
@@ -1,6 +1,23 @@
+import 'functions.dart';
+
 class Implementation {
+  Map<String, double> basicSalaries = {
+    "a": 1000,
+    "b": 2000,
+    "c": 3000,
+    "d": 4000,
+    "e": 5000
+  };
+
   void call() {
-    print('Welcome to implementation of functional programming in Dart');
-    print('Switch between branches to see the code of each lesson');
+    final grossSalaryCalculators = basicSalaries.map((key, value) => MapEntry(
+          key,
+          grossSalaryCalculator(value),
+        ));
+
+    // Convert values to a list
+    print(grossSalaryCalculators["a"]!(80.0));
+    print(grossSalaryCalculators["b"]!(80.0));
+    print(grossSalaryCalculators["c"]!(80.0));
   }
 }
