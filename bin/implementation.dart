e778315f4f143f366f49e7b19106b6e354f67aa7
Amir Bahrawy <Amir@spark-sys.com>
2023-05-18 12:33:19 +0300

    Make stream of data like collections work

--- bin/implementation.dart
+++ bin/implementation.dart
@@ -2,7 +2,7 @@ import 'functions.dart';
 
 class Implementation {
   List<int> data = [2, 1, 3, 6, 9, 10, 11, 13, 18];
-  void call() {
+  void call() async {
     print(
         'Not make pipeline  for all data because collection is optimal and smart');
     data
@@ -12,5 +12,18 @@ class Implementation {
         .where((element) => element > 5)
         .take(2)
         .forEach(print);
+    print('=======================');
+    print(
+        'It will not return all data directly it will return one by one and thats collection work');
+    final fetchData = oneByOne();
+    await for (int i in fetchData) {
+      print(i);
+    }
+  }
+
+  Stream<int> oneByOne() async* {
+    for (int item in data) {
+      yield item;
+    }
   }
 }
