e89ac48a574956997d013d176fb9a868addf7655
Amir Bahrawy <Amir@spark-sys.com>
2023-05-18 12:15:20 +0300

    Lesson ten finished

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,11 +1,14 @@
 int addOne(x) {
+  print('I am adding one');
   return x + 1;
 }
 
 int subtractTen(x) {
+  print('I am Subtracting ten');
   return x - 10;
 }
 
 int square(x) {
+  print('I am Doing as square ');
   return x * x;
 }
--- bin/implementation.dart
+++ bin/implementation.dart
@@ -1,6 +1,16 @@
+import 'functions.dart';
+
 class Implementation {
+  List<int> data = [2, 1, 3, 6, 9, 10, 11, 13, 18];
   void call() {
-    print('Welcome to implementation of functional programming in Dart');
-    print('Switch between branches to see the code of each lesson');
+    print(
+        'Not make pipeline  for all data because collection is optimal and smart');
+    data
+        .map(addOne)
+        .map(square)
+        .map(subtractTen)
+        .where((element) => element > 5)
+        .take(2)
+        .forEach(print);
   }
 }
